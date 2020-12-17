fcdc9e058c569f49a71be6413864cd9cb35b7b98
trishastlouis <wyntrish2020@gmail.com>
2020-12-16 22:56:38 -0500

    Finished User reviews, Chef Replies, and Bookings page models

---
+++ server/db/models/bookingsPage.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookingsPageSchema = new Schema(
+  {
+    isPrefixed: { type: Boolean, required: true, trim: true },
+    cartItems: { type: Object, Array?, default: null },
+    itemQuantity: { type: Number, required: true, default: 0 },
+    mealSetSelection: { type: Object },
+    numberofPeople: { type: Number, required: true, default: 1 },
+    deliveryFee: { type: Number, default: true },
+    totalCost: { type: Number },
+    bookingDate: {type: Number, required: true},
+    bookingTime: { type: Number, required: true},
+    addressConfirmation: { type: String, required: true}
+  },
+  { timestamps: true }
+);
+
+const bookingsPage = mongoose.model('bookingsPage', bookingsPageSchema);
+module.exports = bookingsPageSchema;
