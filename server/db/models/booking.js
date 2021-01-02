5c0e319602fa24a83f0fdb612722b19d2b62ec1f
Peter Byun <byunpeterj@gmail.com>
2021-01-02 16:04:51 -0500

    updated booking schema; syntax and structure

--- server/db/models/booking.js
+++ server/db/models/booking.js
@@ -1,21 +1,59 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const bookingsPageSchema = new Schema(
+const bookingSchema = new Schema(
   {
-    isPrefixed: { type: Boolean, required: true, trim: true },
-    cartItems: [{ type: ObjectId, ref: 'Dish', default: null }],
-    itemQuantity: { type: Number, required: true, default: 0 },
-    mealSetSelection: [{ type: ObjectId, ref: 'MealSet' }],
-    numberofPeople: { type: Number, required: true, default: 1 },
-    deliveryFee: { type: Number, default: true },
-    totalCost: { type: Number },
-    bookingDate: { type: Number, required: true },
-    bookingTime: { type: Number, required: true },
-    addressConfirmation: { type: String, required: true }
+    isPrefixed: {
+      type: Boolean,
+      required: true
+    },
+    cartItems: [
+      {
+        dishItem: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Dish',
+          default: null
+        },
+        numOfDishItem: {
+          type: Number
+        }
+      }
+    ],
+    mealSetCart: [
+      {
+        mealSetSelection: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'MealSet'
+        },
+        numOfAdditionalPeople: {
+          type: Number,
+          default: 0
+        }
+      }
+    ],
+    serviceFee: {
+      type: Number,
+      default: true
+    },
+    totalCost: {
+      type: Number,
+      required: true
+    },
+    bookingDate: {
+      type: Number,
+      required: true
+    },
+    bookingTime: {
+      type: Number,
+      required: true
+    },
+    addressConfirmation: {
+      type: String,
+      required: true
+    }
   },
   { timestamps: true }
 );
 
-const bookingsPage = mongoose.model('bookingsPage', bookingsPageSchema);
-module.exports = bookingsPageSchema;
+const booking = mongoose.model('booking', bookingSchema);
+module.exports = bookingSchema;
