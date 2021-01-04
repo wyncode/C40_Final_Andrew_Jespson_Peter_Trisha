08ee42e8abf5b48fa6996133a4bf8e167fcc6c2f
Jespson Saint-Pierre <jespstpierre@yahoo.com>
2021-01-04 00:03:02 -0500

    routes fixed

--- server/routes/secure/dishRoutes.js
+++ server/routes/secure/dishRoutes.js
@@ -8,17 +8,15 @@ const {
   deleteDish
 } = require('../../controllers/dish');
 
-
 //get all dishes
 router.get('/', getAlldishes);
 //get a single dishes using different queries in slug to find the dish
-router.get('/:slug', getADish);
+router.get('/::id', getADish);
 //create a dish
 router.post('/', createDish);
 //update a dish using different queries in slug to find the dish
-router.put('/slug', updateDish);
+router.put('/:id', updateDish);
 //delete a dish using different queries in slug to find the dish
 router.delete('/:id', deleteDish);
-
 
 module.exports = router;
